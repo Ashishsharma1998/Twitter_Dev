893548bced11e7994b5af681dad9fc13f998c957
Ashish sharma <sharma07101998@gmail.com>
2023-02-07 01:52:43 +0530

    added comment model

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,8 +6,24 @@ const commentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    userEmail: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    onModel: {
       type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+    },
+    commentable: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
+    },
+    comments: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
     },
   },
   { timestamps: true }
